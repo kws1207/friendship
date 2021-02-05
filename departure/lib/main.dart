2b1200a518760098aa06551b4d159e4c8b891aa8
김우성 <kws@gim-useong-ui-MacBookPro.local>
2021-02-05 22:28:38 +0900

    branch test

--- departure/lib/main.dart
+++ departure/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Startup Name Generator?',
       home: RandomWords(),
     );
   }
